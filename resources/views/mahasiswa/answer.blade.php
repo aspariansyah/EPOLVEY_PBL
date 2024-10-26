447cf0367aa27329940edbc17b19f235afc8d367
AndikaJayaNanda <andikajn25@gmail.com>
2024-10-26 19:36:29 +0700

    perbaikan

--- resources/views/mahasiswa/answer.blade.php
+++ resources/views/mahasiswa/answer.blade.php
@@ -14,37 +14,37 @@
                     @endforeach
                 </ul>
             </div>
-        @endif
-        @foreach($survey->questions as $question)
-        <div class="bg-white p-4 rounded-lg shadow mb-4">
-            <h2 class="font-semibold">{{ $question->pertanyaan }}</h2>
-    
-            @if($question->jenis === 'pilihan')
-                @for($i = 1; $i <= 5; $i++)
-                    <label class="block mt-2">
-                        <input type="radio" name="answers[{{ $question->id }}][jawaban]" value="{{ $i }}" class="form-radio"
-                               onchange="saveAnswer({{ $question->id }}, this.value)"
-                               {{ isset($answers[$question->id]) && $answers[$question->id]->skor == $i ? 'checked' : '' }}>
-                        <span class="ml-2">{{ $i }}:
-                            @if($i == 1) Sangat Tidak Puas
-                            @elseif($i == 2) Tidak Puas
-                            @elseif($i == 3) Cukup Puas
-                            @elseif($i == 4) Puas
-                            @elseif($i == 5) Sangat Puas
-                            @endif
-                        </span>
-                    </label>
-                @endfor
-            @elseif($question->jenis === 'essay')
-                <textarea name="answers[{{ $question->id }}][jawaban]" class="form-textarea mt-1 block w-full" rows="3"
-                          onchange="saveAnswer({{ $question->id }}, this.value)"
-                          placeholder="Tulis jawaban Anda di sini...">{{ isset($answers[$question->id]) ? $answers[$question->id]->jawaban : '' }}</textarea>
             @endif
-    
-            <!-- Pastikan untuk menyertakan question_id -->
-            <input type="hidden" name="answers[{{ $question->id }}][question_id]" value="{{ $question->id }}">
-        </div>
-    @endforeach
+            @foreach($survey->questions as $question)
+                <div class="bg-white p-4 rounded-lg shadow mb-4">
+                    <h2 class="font-semibold">{{ $question->pertanyaan }}</h2>
+
+                    @if($question->jenis === 'pilihan')
+                        @for($i = 1; $i <= 5; $i++)
+                            <label class="block mt-2">
+                                <input type="radio" name="answers[{{ $question->id }}][jawaban]" value="{{ $i }}" class="form-radio"
+                                       onchange="saveAnswer({{ $question->id }}, this.value)"
+                                       {{ isset($answers[$question->id]) && $answers[$question->id]->skor == $i ? 'checked' : '' }}>
+                                <span class="ml-2">{{ $i }}:
+                                    @if($i == 1) Sangat Tidak Puas
+                                    @elseif($i == 2) Tidak Puas
+                                    @elseif($i == 3) Cukup Puas
+                                    @elseif($i == 4) Puas
+                                    @elseif($i == 5) Sangat Puas
+                                    @endif
+                                </span>
+                            </label>
+                        @endfor
+                    @elseif($question->jenis === 'essay')
+                        <textarea name="answers[{{ $question->id }}][jawaban]" class="form-textarea mt-1 block w-full" rows="3"
+                                  onchange="saveAnswer({{ $question->id }}, this.value)"
+                                  placeholder="Tulis jawaban Anda di sini...">{{ isset($answers[$question->id]) ? $answers[$question->id]->jawaban : '' }}</textarea>
+                    @endif
+
+                    <!-- Pastikan untuk menyertakan question_id -->
+                    <input type="hidden" name="answers[{{ $question->id }}][question_id]" value="{{ $question->id }}">
+                </div>
+            @endforeach
 
             <div class="text-center">
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Kirim Jawaban</button>
@@ -53,16 +53,20 @@
     </div>
 
     <script>
-        // Simpan jawaban ke local storage
+        // Ambil ID pengguna dan survei dari backend
+        const userId = {{ auth()->user()->id }};
+        const surveyId = {{ $survey->id }};
+
+        // Simpan jawaban ke local storage dengan identifikasi unik
         function saveAnswer(questionId, answer) {
-            let answers = JSON.parse(localStorage.getItem('surveyAnswers')) || {};
+            let answers = JSON.parse(localStorage.getItem(`surveyAnswers_${userId}_${surveyId}`)) || {};
             answers[questionId] = { jawaban: answer };
-            localStorage.setItem('surveyAnswers', JSON.stringify(answers));
+            localStorage.setItem(`surveyAnswers_${userId}_${surveyId}`, JSON.stringify(answers));
         }
 
         // Ambil jawaban dari local storage saat halaman dimuat
         window.onload = function() {
-            let answers = JSON.parse(localStorage.getItem('surveyAnswers')) || {};
+            let answers = JSON.parse(localStorage.getItem(`surveyAnswers_${userId}_${surveyId}`)) || {};
             for (let questionId in answers) {
                 let answer = answers[questionId].jawaban;
                 let input = document.querySelector(`input[name="answers[${questionId}][jawaban]"][value="${answer}"]`);
